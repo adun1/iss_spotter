db373daf974dbd56ad031b88d2e8bdc96bdda069
adun1 <bmccormack15@gmail.com>
2020-06-05 23:47:56 +0000

    finished get ip address

---
+++ index.js
@@ -0,0 +1,16 @@
+const {fetchMyIP} = require('./iss');
+
+const main = function() {
+  /*
+  fetchMyIP((error, ip) => {
+    if (error) {
+      console.log("It didn't work!", error);
+      return;
+    }
+  
+    console.log("it worked! Returned IP:", ip);
+  });
+  */
+
+};
+main();
